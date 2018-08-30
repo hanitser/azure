5183b9b76f3650c2ee8e36f4e79369951a3d092a
aaakhalifa <40789351+aaakhalifa@users.noreply.github.com>
2018-08-31 00:14:51 +0200

    Generalize
    Generalize

--- datalake/1GenerateCertificate.ps1
+++ datalake/1GenerateCertificate.ps1
@@ -1,18 +1,31 @@
-## Certificate creation for SPN authentication
-$certFolder = "C:\certificates"
-$certFilePath = "$certFolder\certFile-dev.pfx"
-$certStartDate = (Get-Date).Date
-$certStartDateStr = $certStartDate.ToString("MM/dd/yyyy")
-$certEndDate = $certStartDate.AddYears(4)
-$certEndDateStr = $certEndDate.ToString("MM/dd/yyyy")
-$certName = "HDI-ADLS-DEV"
-$certPassword = "Alv9A3tSjTGSghsQtvGb"
-$certPasswordSecureString = ConvertTo-SecureString $certPassword -AsPlainText -Force
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory = $true)]
+  [String]$certFolder,
+  [Parameter(Mandatory = $true)]
+  [String]$CertFilePath,
+  [Parameter(Mandatory = $true)]
+  [String]$certName,
+  [Parameter(Mandatory = $true)]
+  [String]$CertPassword,
+  [Parameter()]
+  [int]$validityInYears = 4
+)
 
-mkdir $certFolder
-
-$cert = New-SelfSignedCertificate -DnsName $certName -CertStoreLocation cert:\CurrentUser\My -KeySpec KeyExchange -NotAfter $certEndDate -NotBefore $certStartDate
-$certThumbprint = $cert.Thumbprint
-$cert = (Get-ChildItem -Path cert:\CurrentUser\My\$certThumbprint)
-
-Export-PfxCertificate -Cert $cert -FilePath $certFilePath -Password $certPasswordSecureString
+function Create-AAACertificate {
+   param ($certFolder,$CertFilePath,$certName,$certPassword,$ValidityInYears)
+    ## Certificate creation for SPN authentication
+	$certStartDate = (Get-Date).Date
+	$certStartDateStr = $certStartDate.ToString("MM/dd/yyyy")
+	$certEndDate = $certStartDate.AddYears($ValidityInYears)
+	$certEndDateStr = $certEndDate.ToString("MM/dd/yyyy")
+	$certPasswordSecureString = ConvertTo-SecureString $certPassword -AsPlainText -Force
+	
+	mkdir $certFolder
+	
+	$cert = New-SelfSignedCertificate -DnsName $certName -CertStoreLocation cert:\CurrentUser\My -KeySpec KeyExchange -NotAfter $certEndDate -NotBefore $certStartDate
+	$certThumbprint = $cert.Thumbprint
+	$cert = (Get-ChildItem -Path cert:\CurrentUser\My\$certThumbprint)
+	
+	Export-PfxCertificate -Cert $cert -FilePath $certFilePath -Password $certPasswordSecureString
+}
